fab33708bdc4587c6604b1bc10af7ebb98e06cdb
Shafinaz Shafique <shafinaz2006@gmail.com>
2020-11-04 11:48:27 -0800

    Add files via upload

---
+++ socialMediaDashboard.js
@@ -0,0 +1,17 @@
+const toggleSwitch = document.querySelector('#checkbox');
+const modeSelect = document.querySelector('#modeSelect')
+
+toggleSwitch.addEventListener('change', function() {
+
+    if (this.checked) {
+
+        document.documentElement.setAttribute('data-theme', 'dark');
+        modeSelect.innerText = 'Light Mode';
+
+    } else {
+
+        document.documentElement.setAttribute('data-theme', 'light');
+        modeSelect.innerText = 'Dark Mode';
+
+    }
+})
